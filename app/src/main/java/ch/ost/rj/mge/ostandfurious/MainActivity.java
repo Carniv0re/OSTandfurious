4bad4b02bec815344ceaeac6662354fc72f334b9
Lucas <lucaskvn@gmail.com>
2021-10-14 16:02:02 +0200

    Add intents to switch between activities

--- app/src/main/java/ch/ost/rj/mge/ostandfurious/MainActivity.java
+++ app/src/main/java/ch/ost/rj/mge/ostandfurious/MainActivity.java
@@ -2,7 +2,15 @@ package ch.ost.rj.mge.ostandfurious;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playGameBtn = this.findViewById(R.id.buttonPlayGame);
+        playGameBtn.setEnabled(false);
+
+        Button leaderboardBtn = this.findViewById(R.id.buttonLeaderboard);
+        leaderboardBtn.setOnClickListener(v -> {
+            Intent leaderboardActivityIntent = new Intent (
+                    this,
+                    leaderboardActivity.class
+            );
+            startActivity(leaderboardActivityIntent);
+        });
+
+        Button creditsBtn = this.findViewById(R.id.buttonCredits);
+        creditsBtn.setOnClickListener(v -> {
+            Intent creditsActivityIntent = new Intent(
+                    this,
+                    creditActivity.class
+            );
+            startActivity(creditsActivityIntent);
+        });
+
+        EditText nameEditText = this.findViewById(R.id.editTextYourName);
+        nameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                playGameBtn.setEnabled(true);
+            }
+        });
+
+        playGameBtn.setOnClickListener(v -> {
+            Intent gameActivityIntent = new Intent(
+                    this,
+                    gameActivity.class
+                    );
+            startActivity(gameActivityIntent);
+        });
     }
 }
--- app/src/main/java/ch/ost/rj/mge/ostandfurious/creditActivity.java
+++ app/src/main/java/ch/ost/rj/mge/ostandfurious/creditActivity.java
@@ -2,7 +2,9 @@ package ch.ost.rj.mge.ostandfurious;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class creditActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class creditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credit);
+
+        Button backBtn = this.findViewById(R.id.buttonBack);
+        backBtn.setOnClickListener(view -> {
+            Intent mainActivityIntent = new Intent(
+                    this,
+                    MainActivity.class
+            );
+            startActivity(mainActivityIntent);
+        });
     }
 }
